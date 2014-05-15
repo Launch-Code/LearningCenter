c4b537f17ab4b9136c96cacc3b33500780140b88
chiangchendah <chiangchendah@gmail.com>
2014-05-15 15:42:54 -0500

    Content Added

--- assets/js/choices.js
+++ assets/js/choices.js
@@ -29,7 +29,7 @@ window['launch-data'] = [
             },
             {
                 "name": "Mobile",
-                "type": "info disabled"
+                "type": "info"
             }
         ],
         "resources": null,
@@ -61,7 +61,7 @@ window['launch-data'] = [
             },
             {
                 "name": "No",
-                "type": "info disabled"
+                "type": "info"
             }
         ],
         "resources": null,
@@ -81,44 +81,114 @@ window['launch-data'] = [
             },
             {
                 "name": "Node.js",
-                "type": "info disabled"
+                "type": "info"
             },
             {
                 "name": "Java (Spring)",
-                "type": "info disabled"
+                "type": "info"
             },
             {
                 "name": "C# (MVC)",
-                "type": "info disabled"
+                "type": "info"
             }
         ],
         "resources": null,
         "from": "3:0"
     },
     {
-        "key": "5",
+        "key": "",
         "question": null,
         "choices": null,
         "resources": [
             {
                 "title": "PHP",
-                "description": "PHP is a mature language that is widely known, and incredibly easy to get started with",
+                "description": "PHP is a mature language that is widely known, and incredibly easy to get started with.",
                 "links": [
                     {
-                        "description": "Google",
-                        "href": "http://google.com"
-                    },
-                    {
-                        "description": "Php.net Manual",
+                        "description": "PHP Manual",
                         "href": "http://www.php.net/manual/en/"
                     }
                 ]
-            }
+            },
+            {
+                "title": "Beginner PHP",
+                "description": null,
+                "links": [
+                    {
+                        "description": "Codecademy PHP Track",
+                        "href": "http://www.codecademy.com/tracks/php"
+                    }
+                ]
+            },
         ],
         "from": "4:0"
     },
     {
-        "key": "6",
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "Java",
+                "description": "Java is a class based, object-oriented programming language that is in high demand.",
+                "links": [
+                    {
+                        "description": "Head First Java",
+                        "href": "http://www.amazon.com/Head-First-Java-2nd-Edition/dp/0596009208"
+                    },
+                    {
+                        "description": "Effective Java",
+                        "href": "http://www.amazon.com/Effective-Java-Edition-Joshua-Bloch/dp/0321356683/ref=sr_1_3?ie=UTF8&qid=1398733577&sr=8-3&keywords=java"
+                    }
+                ]
+            },
+            {
+                "title": "Beginner Java",
+                "description": null,
+                "links": [
+                    {
+                        "description": "Learn Java",
+                        "href": "http://www.learnjavaonline.org/"
+                    }
+                ]
+            }
+        ],
+        "from": "5:0"
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "Android",
+                "description": "Android is a popular operating system engineered by Google for touch screen mobile devices.",
+                "links": [
+                    {
+                        "description": "Android Documentation",
+                        "href": "https://developer.android.com/guide/index.html"
+                    },
+                    {
+                        "description": "Android Developer Studio (Recommended Integrated Development Environment)",
+                        "href": "http://developer.android.com/sdk/installing/studio.html"
+                    }
+                ]
+            },
+            {
+                "title": "Beginner Android",
+                "description": null,
+                "links": [
+                    {
+                        "description": "Introductory Android Github Project",
+                        "href": "https://github.com/kousen/HelloWorld/"
+                    }
+                ]
+            }
+        ],
+        "from": "6:0"
+    },
+    {
+        "key": "",
         "question": null,
         "choices": [],
         "resources": [
@@ -136,27 +206,27 @@ window['launch-data'] = [
         "from": "0:1"
     },
     {
-        "key": "7",
+        "key": "5",
         "question": "What language do you want to write in?",
         "choices": [
             {
                 "name": "Java",
-                "type": "info disabled"
+                "type": "info"
             },
             {
                 "name": "C#",
-                "type": "info disabled"
+                "type": "info"
             },
             {
                 "name": "C/C++",
-                "type": "info disabled"
+                "type": "info"
             }
         ],
         "resources": null,
         "from": "1:1"
     },
     {
-        "key": "8",
+        "key": "",
         "question": null,
         "choices": [],
         "resources": [
@@ -165,20 +235,325 @@ window['launch-data'] = [
                 "description": "HTML5 and CSS3 will allow you to design beautiful, modern websites.",
                 "links": [
                     {
-                        "description": "Dive into HTML5",
-                        "href": "http://diveintohtml5.info/"
-                    },
-                    {
                         "description": "CSS Tricks",
                         "href": "http://css-tricks.com/"
                     },
                     {
-                        "description": "CodePen - Live examples",
+                        "description": "Live Examples from CodePen",
                         "href": "http://codepen.io/"
                     }
                 ]
             }
         ],
         "from": "2:1"
-    }
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "HTML",
+                "description": "HTML or Hyper Text Markup Language is the standard markup language used to create web pages.",
+                "links": [
+                    {
+                        "description": "Dive into HTML5",
+                        "href": "http://diveintohtml5.info/"
+                    },
+                    {
+                        "description": "W3School",
+                        "href": "http://www.w3schools.com/html/default.asp"
+                    }
+                ]
+            },
+            {
+                "title": "CSS",
+                "description": "CSS or Cascading Style Sheets is a style sheet language used to describe the looks of a HTML document.",
+                "links": [
+                    {
+                        "description": "Codecademy Web Track",
+                        "href": "http://www.codecademy.com/tracks/web"
+                    },
+                    {
+                        "description": "Twitter Bootstrap on Github",
+                        "href": "https://github.com/twbs/bootstrap"
+                    }
+                ]
+            },
+            {
+                "title": "JavaScript",
+                "description": "JavaScript is a dynamic programming language commonly used as a part of web browsers for client-side interactions.",
+                "links": [
+                    {
+                        "description": "JavaScript: The Good Parts",
+                        "href": "http://www.amazon.com/JavaScript-Good-Parts-Douglas-Crockford/dp/0596517742"
+                    },
+                    {
+                        "description": "Codecademy JavaScript Track",
+                        "href": "http://www.codecademy.com/tracks/javascript"
+                    },
+                    {
+                        "description": "Secrets of the JavaScript Ninja by John Resig (Creator of jQuery)",
+                        "href": "http://www.amazon.com/Secrets-JavaScript-Ninja-John-Resig/dp/193398869X"
+                    },
+                    {
+                        "description": "Tutorials to be Used with Secrets of the JavaScript Ninja",
+                        "href": "http://ejohn.org/apps/learn/"
+                    }
+                ]
+            }
+        ],
+        "from": "3:1"   
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources":[
+            {
+                "title": "Ruby (on Rails)",
+                "description": "Ruby (on Rails) is a framework for website building, where your website is already half done for you.",
+                "links": [
+                    {
+                        "description": "Ruby Documentation",
+                        "href": "http://ruby-doc.org/"
+                    },
+                    {
+                        "description": "Why's (poignant) Guide to Ruby",
+                        "href": "http://www.rubyinside.com/media/poignant-guide.pdf"
+                    },
+                    {
+                        "description": "Learn Ruby the Hard Way",
+                        "href": "http://ruby.learncodethehardway.org/book/"
+                    },
+                    {
+                        "description": "Metaprogramming Ruby",
+                        "href": "http://pragprog.com/book/ppmetr/metaprogramming-ruby"
+                    }
+                ]
+            },
+            {
+                "title": "Beginner Ruby",
+                "description": null,
+                "links": [
+                    {
+                        "description": "Learn the Basics of Ruby in Your Browser",
+                        "href": "http://tryruby.org"
+                    },
+                    {
+                        "description": "Codecademy Ruby Track",
+                        "href": "http://www.codecademy.com/tracks/ruby"
+                    },
+                    {
+                        "description": "Ruby in 20 Minutes",
+                        "href": "https://www.ruby-lang.org/en/documentation/quickstart/"
+                    },
+                    {
+                        "description": "Michael Hartl Rail Tutorial",
+                        "href": "http://www.railstutorial.org/book"
+                    }
+                ]
+            },
+            {
+                "title": "Intermediate Ruby",
+                "description": null,
+                "links": [
+                    {
+                        "description": "Study Notes by Satish Talim",
+                        "href": "http://rubylearning.com/"
+                    },
+                    {
+                        "description": "Ruby Tapas",
+                        "href": "http://rubytapas.com"
+                    },
+                    {
+                        "description": "Ruby Weekly",
+                        "href": "http://rubyweekly.com/"
+                    }
+                ]
+            },
+            {
+                "title": "Advanced Ruby",
+                "description": null,
+                "links": [
+                    {
+                        "description": "GoRails",
+                        "href": "https://gorails.com/"
+                    },
+                    {
+                        "description": "Railscasts",
+                        "href": "http://railscasts.com/"
+                    }
+                ]
+            }
+        ],
+        "from": "4:1"
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "C#",
+                "description": "C# is a programming language similar to Java.",
+                "links": [
+                    {
+                        "description": "Microsoft Developer Network Programming Guide",
+                        "href": "http://msdn.microsoft.com/en-us/library/67ef8sbd.aspx%20"
+                    },
+                    {
+                        "description": "Zetcode C# Tutorials",
+                        "href": "http://zetcode.com/lang/csharp/"
+                    }
+                ]
+            }
+        ],
+        "from": "5:1"
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "iOS",
+                "description": "iOS is an operating system developed by Apple exclusively for their hardware.",
+                "links": [
+                    {
+                        "description": "iOS Developer Library",
+                        "href": "https://developer.apple.com/library/ios/navigation/"
+                    }
+                ]
+            },
+            {
+                "title": "Beginner iOS",
+                "description": null,
+                "links": [
+                    {
+                        "description": "iOS Development from Stanford",
+                        "href": "https://itunes.apple.com/us/course/developing-ios-7-apps-for/id733644550"
+                    }
+                ]
+            },
+            {
+                "title": "Intermediate iOS",
+                "description": null,
+                "links": [
+                    {
+                        "description": "Ray Wenderlich's Blog - High Quality Tutorials",
+                        "href": "http://www.raywenderlich.com/"
+                    }
+                ]
+            },
+            {
+                "title": "Advanced iOS",
+                "description": null,
+                "links": [
+                    {
+                        "description": "NSHipster - Discussions on iOS Development",
+                        "href": "http://nshipster.com"
+                    }
+                ]
+            }
+        ],
+        "from": "6:1"
+    },
+    {
+        "key": "6",
+        "question": "Which mobile operating system are you interested in?",
+        "choices": [
+            {
+                "name": "Android",
+                "type": "info"
+            },
+            {
+                "name": "iOS",
+                "type": "info"
+            }
+        ],
+        "resources": null,
+        "from": "1:2"
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "Node.js",
+                "description": "Node.js is a lightweight server-side proxy",
+                "links": [
+                    {
+                        "description": "RESTful Node.js Mongodb App",
+                        "href": "http://coenraets.org/blog/2012/10/creating-a-rest-api-using-node-js-express-and-mongodb/"
+                    }
+                ]
+            }
+        ],
+        "from": "4:2"
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "C/C++",
+                "description": "C++ is an intermediate-level language with object-oriented programming features to enhance the C langauge.",
+                "links": [
+                    {
+                        "description": "CPlusPlus",
+                        "href": "http://www.cplusplus.com/doc/tutorial/"
+                    }
+                ]
+            }
+        ],
+        "from": "5:2"
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "Java (Spring)",
+                "description": "Spring is a lightweight inversion of control and aspect-oriented container framework.",
+                "links": [
+                    {
+                        "description": "Getting Started",
+                        "href": "http://projects.spring.io/spring-boot/#quick-start"
+                    },
+                    {
+                        "description": "Getting Going with Spring",
+                        "href": "https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-simple"
+                    },
+                    {
+                        "description": "Building Your First REST Server",
+                        "href": "https://spring.io/guides/gs/rest-service/"
+                    }
+                ]
+            }
+        ],
+        "from": "4:3"
+    },
+    {
+        "key": "",
+        "question": null,
+        "choices": [],
+        "resources": [
+            {
+                "title": "C# (MVC)",
+                "description": "A web application framework from Microsoft.",
+                "links": [
+                    {
+                        "description": "ASP.NET MVC",
+                        "href": "http://www.asp.net/mvc"
+                    }
+                ]
+            }
+        ],
+        "from": "4:4"
+    },
+    
 ];
